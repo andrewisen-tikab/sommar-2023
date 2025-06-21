95b0296babe64d342dde328afe1c0358dd78fa64
André "MG" Wisén <andre.wisen@gmail.com>
2025-06-21 09:40:23 +0200

    Remove PWA auto-update registration from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [splitVendorChunkPlugin(), react(), VitePWA({ registerType: 'autoUpdate' })],
+    plugins: [splitVendorChunkPlugin(), react()],
 });
